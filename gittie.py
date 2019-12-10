245215d6de24ff1c7d4972eed4e69feb77fa95a1
Bocian <rysiek.gornowicz@gmail.com>
2019-12-10 11:00:11 +0100

    I have modified the introduction function

--- gittie.py
+++ gittie.py
@@ -1,13 +1,11 @@
 def f_introduce():
     print('Hello!')
-<<<<<<< HEAD
+    Print("Let's see if it crashes")
 
 def f_add(a,b):
     result = a + b
     return result 
-=======
     
 def f_joke():
     print("hahahah, what a great joke")
 
->>>>>>> 7f5366df6998a41772c44670a7f34e8574fe8887
