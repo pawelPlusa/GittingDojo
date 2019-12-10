df2cb07dd1a454445d388754d189a6b8c23bde4f
corumov <ppplusa@gmail.com>
2019-12-10 10:59:16 +0100

    Changed introduce function

--- gittie.py
+++ gittie.py
@@ -1,13 +1,11 @@
 def f_introduce():
     print('Hello!')
-<<<<<<< HEAD
+    print("my name is Pawel")
 
 def f_add(a,b):
     result = a + b
     return result 
-=======
     
 def f_joke():
     print("hahahah, what a great joke")
 
->>>>>>> 7f5366df6998a41772c44670a7f34e8574fe8887
